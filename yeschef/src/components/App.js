a453e83690eb9f81e215a9e8284c56cea6308233
Nick Johnson <nlj222@gmail.com>
2023-02-13 15:05:55 -0800

    json server working port 3001

--- yeschef/src/components/App.js
+++ yeschef/src/components/App.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import RecipeList from "./RecipeList"
 import NewRecipeForm from "./NewRecipeForm";
 import NavBar from "./NavBar";
 
 function App(){
+
+    const [recipes, setRecipes] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:4000/recipes")
+          .then((resp) => resp.json())
+          .then((recipes) => setRecipes(recipes));
+      }, []);
+
     return (
         <div>
             <NavBar />
