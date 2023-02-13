38475d485a31cfe4f3ee6e6de7b6e0b4be742a88
Nick Johnson <nlj222@gmail.com>
2023-02-13 10:45:06 -0800

    starter code setup

--- src/components/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function App(){
-    return <p>App is now connected</p>
-}
-
-export default App
---
+++ yeschef/src/components/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App(){
+    return (<p>App is now connected</p>)
+}
+
+export default App
