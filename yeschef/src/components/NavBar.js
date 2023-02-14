3c54f5cfccfcb263302ae806fd3f4de5b48ea842
Nick Johnson <nlj222@gmail.com>
2023-02-14 10:07:53 -0800

    built basic navbar

--- yeschef/src/components/NavBar.js
+++ yeschef/src/components/NavBar.js
@@ -1,6 +1,18 @@
+import {Link, NavLink} from 'react-router-dom';
+import Search from './Search';
+
 function NavBar(){
     return(
-        <p>nav bar</p>
+        <header>
+            <nav>
+                <Link to="/"><h1 className="title">yes, chef.</h1></Link>
+                <Search />
+                <div className="navigation">
+                    <NavLink className="button" exact to="/recipes">All Recipes</NavLink>
+                    <NavLink className="button" exact to="/recipes/new">Add A New Recipe</NavLink>
+                </div>
+            </nav>
+        </header>
     )
 }
 
