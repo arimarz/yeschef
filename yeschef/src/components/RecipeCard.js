ae625080075f964c5a4b85a3e1fac14e70b63afe
Ari <aritheriver@gmail.com>
2023-02-15 10:47:08 -0800

    favorite button workinggit add .git add .!

--- yeschef/src/components/RecipeCard.js
+++ yeschef/src/components/RecipeCard.js
@@ -1,14 +1,38 @@
-// import { Card } from "semantic-ui-react";
+import {useState} from 'react'
 
-function RecipeCard({ name, image, cuisine, onClick}) {
+function RecipeCard({ name, image, cuisine, onSwitch, favorited, id}) {
+  const [isFavorite, setFavorite] = useState(favorited)
+
+    function handleFavorite() {
+    setFavorite((isFavorite)=> !isFavorite)
+
+    fetch(`http://localhost:3001/recipes/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        favorited: isFavorite
+      })
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error))
+  }
+
+
   return (
-      <div className="recipe-card-content" onClick={onClick}>
+      <div>
+      <div className="recipe-card-content" onClick={onSwitch}>
         <h2>{name}</h2>
         <img className="image" src={image} alt={name} />
         <p>
           <strong>Cuisine:</strong> {cuisine}
         </p>
       </div>
+        <button onClick={handleFavorite}>
+          {isFavorite ? "UNFAVORITE" : "FAVORITE"}</button>
+          </div>
   );
 }
 
